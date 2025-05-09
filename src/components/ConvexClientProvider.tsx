01e701199707da9d463c50f492629ecf19c57bd6
Luca1905 <contact@lucawang.me>
2025-05-09 18:27:28 +0200

    added t3 env

--- src/components/ConvexClientProvider.tsx
+++ src/components/ConvexClientProvider.tsx
@@ -4,7 +4,9 @@ import { ReactNode } from "react";
 import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { useAuth } from "@clerk/nextjs";
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+import { env } from "@/env";
+
+const convex = new ConvexReactClient(env.NEXT_PUBLIC_CONVEX_URL!);
 
 export default function ConvexClientProvider({
   children,
